a981a18d8d50599bb991fabb91f8e7d6619d75b1
davidjeg <davideg96@outlook.com>
2026-07-24 16:40:01 -0500

    fix type icons

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,7 +1,7 @@
 import Types from "./Types.js";
 export default function PokemonCard({ name, types, id }) {
   const idFormateado = id < 1000 ? String(id).padStart(4, "0") : String(id);
-  return `<a href="../pages/pokemonDetail.html?name=${name.toLowerCase()}" class="pokemon__card">
+  return `<a href="pages/pokemonDetail.html?name=${name.toLowerCase()}" class="pokemon__card">
 
 <div class="pokemon__image-container">
     <img class="pokemon__img" src="https://pokedex.hybridshivam.com/assets/thumbnails-compressed/${idFormateado}.png" />
